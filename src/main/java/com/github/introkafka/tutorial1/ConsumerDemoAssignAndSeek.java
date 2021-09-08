a9d812e96a12b1f13d84f99de8e5af1c16b95cbd
Ana Escobar <anaescobar8@gmail.com>
2021-09-08 17:59:00 +0200

    Consumer examples

---
+++ src/main/java/com/github/introkafka/tutorial1/ConsumerDemoAssignAndSeek.java
@@ -0,0 +1,75 @@
+package com.github.introkafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemoAssignAndSeek {
+
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignAndSeek.class.getName());
+
+        String bootstrapServers = "127.0.0.1:9092";
+        // String groupId = "my-seven-app";
+        String topic = "first_topic";
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // earliest/latest/none
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // subscribe consumer to our topic(s)
+        // consumer.subscribe(Collections.singleton(topic));
+        // consumer.subscribe(Arrays.asList(topic));
+
+        // assign and seek are mostly used to replay data or fetch a specific message
+
+        // assign
+        TopicPartition partitionToReadFrom = new TopicPartition(topic, 0);
+        long offsetToReadFrom = 15L;
+        consumer.assign(Arrays.asList(partitionToReadFrom));
+
+        //seek
+        consumer.seek(partitionToReadFrom, offsetToReadFrom);
+
+        int numberOfMessagesToRead = 5;
+        boolean keepOnReading = true;
+        int readMessages = 0;
+
+        // poll for new data
+        while(keepOnReading){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100)); // new in kafka 2.0.0
+
+            for (ConsumerRecord<String, String> record : records){
+                readMessages ++;
+                logger.info("Key: " + record.key() + " Value: " + record.value());
+                logger.info("Partition: " + record.partition() + " Offset: " + record.offset());
+                if(readMessages >= numberOfMessagesToRead){
+                    keepOnReading = false; // to exit the while loop
+                    break; // to exit the for loop
+                }
+            }
+        }
+
+        logger.info("Exiting the application");
+
+    }
+}
