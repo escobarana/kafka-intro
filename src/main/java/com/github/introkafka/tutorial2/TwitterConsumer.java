73c5c851457f8cff864eb25c1d99004134bd03c2
Ana Escobar <anaescobar8@gmail.com>
2021-09-08 18:17:09 +0200

    First commit twitter project

---
+++ src/main/java/com/github/introkafka/tutorial2/TwitterConsumer.java
@@ -0,0 +1,8 @@
+package com.github.introkafka.tutorial2;
+
+public class TwitterConsumer {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/github/introkafka/tutorial2/TwitterProducer.java
@@ -0,0 +1,8 @@
+package com.github.introkafka.tutorial2;
+
+public class TwitterProducer {
+
+    public static void main(String[] args) {
+
+    }
+}
